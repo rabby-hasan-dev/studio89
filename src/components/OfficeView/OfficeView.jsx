b30ba28f9775339d0528b276a7f72148905f1574
rabby-hasan <rabby.65301@gmail.com>
2024-05-30 19:31:41 +0600

    added photo gellary

--- src/components/OfficeView/OfficeView.jsx
+++ src/components/OfficeView/OfficeView.jsx
@@ -1,17 +1,34 @@
 import Map from "../Map/Map";
 
+import Image from "next/image";
+import photo1 from '../../assets/gallery/pohoto (1).jpg';
+import photo2 from '../../assets/gallery/pohoto (2).jpg';
+import photo3 from '../../assets/gallery/pohoto (3).jpg';
+import photo4 from '../../assets/gallery/pohoto (4).jpg';
+import photo5 from '../../assets/gallery/pohoto (5).jpg';
+import photo6 from '../../assets/gallery/pohoto (6).jpg';
+
 const OfficeView = () => {
     return (
         <div className='uppercase text-center m-20 p-10'>
             <h2 className="text-4xl font-bold  my-12 ">Office View</h2>
-            <div className="flex justify-around gap-8 text-yellow-300 ">
-                <div className="bg-slate-500 w-full  rounded-lg hover:bg-blue-600 duration-300 ">
+            <div className="flex justify-around items-center gap-8 text-yellow-300 ">
+                <div className=" w-full  rounded-lg">
 
                     <Map></Map>
 
                 </div>
-                <div className="bg-slate-500  w-full  rounded-lg hover:bg-blue-600  duration-300  ">
-                    <h2 className="text-xl">studio video</h2>
+                <div className="  w-full  rounded-lg  ">
+                    <div className="grid grid-cols-2 gap-0  ">
+                        <Image src={photo1} alt="gellary"></Image>
+                        <Image src={photo2} alt="gellary"></Image>
+                        <Image src={photo3} alt="gellary"></Image>
+                        <Image src={photo4} alt="gellary"></Image>
+                        <Image src={photo5} alt="gellary"></Image>
+                        <Image src={photo6} alt="gellary"></Image>
+                    </div>
+
+
                 </div>
 
             </div>
