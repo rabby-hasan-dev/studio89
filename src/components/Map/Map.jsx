ff8d4212aa81b4f0cc370b6bcbde218aa28a805a
rabby-hasan <rabby.65301@gmail.com>
2024-06-02 12:07:18 +0600

    added map merker icon

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,20 @@
 'use client'
 
 import 'leaflet/dist/leaflet.css';
-
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+
 const Map = () => {
     const position = [23.820626491316503, 90.37038287534364]
+    const myIcon = L.icon({
+        iconUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFYCzrT8ZAbe_J0mAA1tAVPjm5zA4i7r4jw&s',
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        shadowUrl: 'my-icon-shadow.png',
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    });
     return (
 
         <div className='m-0 p-0'>
@@ -13,7 +23,7 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={position} icon={myIcon} >
                     <Popup>
                         Mirpur,Dhaka 1216, Dhaka, Bangladesh
                     </Popup>
